cef6509e2946b8f5b5144c8e12b44c6f55149bc2
JontyWang <501211312@qq.com>
2020-10-17 20:02:05 +0800

    Stack:hammer:

---
+++ 02栈/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02栈
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
