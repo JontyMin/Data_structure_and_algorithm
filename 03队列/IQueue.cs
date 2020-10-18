d037bf6314c9c09ba35f745b0f54a196eb33bbce
JontyWang <501211312@qq.com>
2020-10-18 11:25:25 +0800

    BCL Queue :new:

---
+++ 03队列/IQueue.cs
@@ -0,0 +1,20 @@
+﻿namespace _03队列
+{
+    public interface IQueue<T>
+    {
+        int Count { get; }
+
+        int GetLength();
+
+        bool IsEmpty();
+
+        void Clear();
+
+        void EnQueue(T item);
+
+        T DeQueue();
+
+        T Peek();
+
+    }
+}
---
+++ 03队列/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03队列
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // 1、使用BCL Queue
+            Queue<int>queue=new Queue<int>();
+
+            // 入队
+            queue.Enqueue(12);
+            queue.Enqueue(44);
+            queue.Enqueue(23);
+            queue.Enqueue(45);
+            queue.Enqueue(75);
+
+            Console.WriteLine($"入队列Count:{queue.Count}");
+
+            // 出队 取队首的数据
+            var a = queue.Dequeue(); // 先入先出 12
+            Console.WriteLine(a);
+
+            Console.WriteLine($"出队列Count:{queue.Count}");
+
+            int b = queue.Peek();
+            Console.WriteLine(b);
+
+            Console.WriteLine($"Peek:{queue.Count}");
+
+            queue.Clear();
+
+            Console.WriteLine($"Clear:{queue.Count}");
+
+
+        }
+    }
+}
