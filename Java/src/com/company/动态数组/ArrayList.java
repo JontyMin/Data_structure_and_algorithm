38cb889c2267b9938cee48b69b826dfa840a173b
Jonty <501211312@qq.com>
2021-09-06 20:46:32 +0800

    动态数组接口

---
+++ Java/src/com/company/动态数组/ArrayList.java
@@ -0,0 +1,57 @@
+package com.company.动态数组;
+
+public class ArrayList implements IArrayList {
+
+
+    private int size;
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(int element) {
+        return false;
+    }
+
+    @Override
+    public void add(int element) {
+
+    }
+
+    @Override
+    public int get(int index) {
+        return 0;
+    }
+
+    @Override
+    public int set(int index, int element) {
+        return 0;
+    }
+
+    @Override
+    public int add(int index, int element) {
+        return 0;
+    }
+
+    @Override
+    public int remove(int index) {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(int element) {
+        return 0;
+    }
+}
---
+++ Java/src/com/company/动态数组/demo01.java
@@ -0,0 +1,10 @@
+package com.company.动态数组;
+
+public class demo01 {
+    public static void main(String[] args) {
+        int arr [] = new int[]{12,42,54};
+
+        ArrayList a= new ArrayList();
+
+    }
+}
